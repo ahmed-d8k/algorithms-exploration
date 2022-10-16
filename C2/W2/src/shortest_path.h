b33dc3aa026285e33d006626a94cfd7d3b5ba821
AhmedAmrAwadallahK8 <AwadallahAhmed25@gmail.com>
2022-10-15 23:04:37 -0700

    Setting up algo data structures

--- C2/W2/src/shortest_path.h
+++ C2/W2/src/shortest_path.h
@@ -1,11 +1,17 @@
 #ifndef SHORTEST_PATH_H
 #define SHORTEST_PATH_H
 
+#include <queue>
+#include <map>
+
 #include "../../../DistanceGraph/src/distance_graph.h"
+#include "../../../DistanceGraph/src/vertex.h"
 
 class Shortest_Path{
     private:
         Distance_Graph& graph;
+        std::map<Vertex*, int> discovered_vert_map; 
+        std::queue<Vertex*> q;
     public:
         Shortest_Path(Distance_Graph& g);
         int get_shortest_path_distance(int source_id, int targ_id);
